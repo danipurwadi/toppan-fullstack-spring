9012c053e9ab0ccf0bf66a145a498d54947aaf79
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2023-03-05 19:52:36 +0800

    Update Id to use AuthorId

--- backend/src/main/java/com/dani/repository/AuthorBookRepository.java
+++ backend/src/main/java/com/dani/repository/AuthorBookRepository.java
@@ -1,9 +1,15 @@
 package com.dani.repository;
 
+import com.dani.model.Author;
 import com.dani.model.AuthorBook;
+import com.dani.model.AuthorBookId;
+import com.dani.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
 @Repository
-public interface AuthorBookRepository extends JpaRepository<AuthorBook, Integer> {
+public interface AuthorBookRepository extends JpaRepository<AuthorBook, AuthorBookId> {
 }
