e332dc711e3655aa049de2d25f618810547a174a
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2023-03-09 19:55:39 +0800

    Create state for action button update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './App.css';
-import MainPage from './MainPage';
+import ActionButton from './components/ActionButton';
+import MainPage from './pages/MainPage';
 
 function App() {
+  const [countryCode, setCountryCode] = useState("SG");
+
+  const randomiseCountry = () => {
+    return "UK";
+  };
   return (
     <div className="App">
+      <ActionButton
+        countryCode={countryCode}
+        onChange={() => setCountryCode(randomiseCountry())}
+      />
       <MainPage />
     </div>
   );
